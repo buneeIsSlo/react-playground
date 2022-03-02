cd59400c33d20fe4bf44faafda40f2258f4ff657
bunee <buneeisslo@gmail.com>
2022-03-02 22:25:08 +0530

    learning jsx

--- test/src/App.js
+++ test/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const text = "Wow I love react.";
+  const link = "https://reactjs.org/";
+  const obj = { a: "does", b: "this", c: "work" };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1 className="title">Hello React</h1>
+        <p className="text">{text}</p>
+        <p className="random">{~~(Math.random() * 10)}</p>
+        <a href={link} className="link" target="blank">Take me to React</a>
+        {/* <p className="obj">{obj}</p> */}
+      </div>
     </div>
   );
 }

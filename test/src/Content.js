da79b7c93e56b903d7c7b50aea14c13f0c9ace05
bunee <buneeisslo@gmail.com>
2022-03-03 22:32:46 +0530

    add styling and learn click events

--- test/src/Content.js
+++ test/src/Content.js
@@ -1,15 +1,21 @@
 const Content = () => {
     const text = "Wow I love react.";
     const link = "https://reactjs.org/";
-    const obj = { a: "does", b: "this", c: "work" };
+
+    let num = 0;
+    const rng = () => {
+        num += ~~(Math.random() * 10);
+        console.log(num);
+    }
+
 
     return (
         <div className="content">
             <h1 className="title">Hello React</h1>
             <p className="text">{text}</p>
-            <p className="random">{~~(Math.random() * 10)}</p>
+            <button className="button" onClick={rng}>Generate Random Number</button>
+            <p className="random">{num}</p>
             <a href={link} className="link" target="blank">Take me to React</a>
-            {/* <p className="obj">{obj}</p> */}
         </div>
     )
 }

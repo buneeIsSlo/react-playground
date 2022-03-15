72c28c1b4fa4b7a0939bd225dae4cd0c784794fe
bunee <buneeisslo@gmail.com>
2022-03-15 17:50:23 +0530

    Completed lesson 12

---
+++ blog/src/BlogList.js
@@ -0,0 +1,18 @@
+const BlogList = ({ blogs, title }) => {
+
+    console.log(title);
+
+    return (
+        <div className="blog-list">
+            <h2>{title}</h2>
+            {blogs.map((blog) =>
+                <div className="blog-preview" key={blog.id}>
+                    <h2 className="title">{blog.title}</h2>
+                    <p className="author">{blog.author}</p>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default BlogList;
--- blog/src/Home.js
+++ blog/src/Home.js
@@ -1,24 +1,17 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-
-    let [name, setName] = useState("cell");
-
-    const handleClick = (e) => {
-        console.log("hello world", e);
-    }
-
-    const handleClickAgain = (name, e) => {
-        console.log('hello ' + name, e.target);
-        setName("bunee");
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
 
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name}</p>
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => handleClickAgain('bruva', e)}>Click me again</button>
+            < BlogList blogs={blogs} title={"All blogs"} />
+            < BlogList blogs={blogs.filter((b) => b.author === "mario")} title={"Mario's blogs"} />
         </div>
     );
 }

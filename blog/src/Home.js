c80488319abb1a0f1ac37f37226bc8af5b63b556
bunee <buneeisslo@gmail.com>
2022-03-21 19:11:50 +0530

    completed lesson 15

--- blog/src/Home.js
+++ blog/src/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
 
+    const [name, setName] = useState("mario");
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter((blog) => blog.id !== id);
         setBlogs(newBlogs);
@@ -23,11 +25,14 @@ const Home = () => {
 
     useEffect(() => {
         console.log("useEffect ran");
-    })
+        console.log(name);
+    }, [name]);
 
     return (
         <div className="home">
             < BlogList blogs={blogs} title={"All blogs"} handleDelete={handleDelete} bookmark={bookmark} />
+            <button className="change-name" onClick={() => setName("luigi")}>Change Name</button>
+            <p className="name">{name}</p>
         </div>
     );
 }

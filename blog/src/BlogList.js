42b572f39ccaf5d8b96e9da562d5b4f1efd6c5ef
bunee <buneeisslo@gmail.com>
2022-04-06 20:14:38 +0530

    completed lesson 25

--- blog/src/BlogList.js
+++ blog/src/BlogList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({ blogs, title, handleDelete, bookmark }) => {
 
 
@@ -6,10 +8,12 @@ const BlogList = ({ blogs, title, handleDelete, bookmark }) => {
             <h2>{title}</h2>
             {blogs.map((blog) =>
                 <div className="blog-preview" key={blog.id}>
-                    <h2 className="title">{blog.title}</h2>
-                    <p className="author">{blog.author}</p>
-                    <button className="delete" onClick={() => handleDelete(blog.id)}>Delete</button>
-                    <button className="change" onClick={() => bookmark(blog.id)}>Bookmark</button>
+                    <Link to={`/blog/${blog.id}`}>
+                        <h2 className="title">{blog.title}</h2>
+                        <p className="author">{blog.author}</p>
+                        <button className="delete" onClick={() => handleDelete(blog.id)}>Delete</button>
+                        <button className="change" onClick={() => bookmark(blog.id)}>Bookmark</button>
+                    </Link>
                 </div>
             )}
         </div>

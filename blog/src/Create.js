41124de473d539d600befe51246d4471ecccfc94
bunee <buneeisslo@gmail.com>
2022-04-08 20:28:49 +0530

    completed lesson 28

--- blog/src/Create.js
+++ blog/src/Create.js
@@ -5,10 +5,17 @@ const Create = () => {
     const [body, setBody] = useState("");
     const [author, setAuthor] = useState("");
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+
+        console.log(blog);
+    }
+
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form className="create__form">
+            <form className="create__form" onSubmit={handleSubmit}>
                 <label>Blog title:</label>
                 <input type="text" value={title} onChange={e => setTitle(e.target.value)} />
                 <label>Blog body:</label>

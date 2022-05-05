bb9e2f6dbf3af45a02f1764f2eb2fe66a975a4a4
bunee <buneeisslo@gmail.com>
2022-05-05 22:56:40 +0530

    Add: rating component

--- interactive-rating-comopnent/src/components/Card.js
+++ interactive-rating-comopnent/src/components/Card.js
@@ -1,13 +1,59 @@
+import { useState } from "react";
+
 import images from "../constants/images";
 import "./Card.scss";
 
-const rating = 4;
-const maxRating = 5;
 
-const title = "Hello world";
-const description = "Description is great";
+const content = {
+    title: "How did we do?",
+    description: "Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!",
+    maxRating: 5
+}
 
-const Card = () => {
+const RatingComponent = ({ num, handler, rating }) => {
+    return (
+        <button
+            type="button"
+            className={`card__rating-btn${rating === num ? " active" : ""}`}
+            onClick={() => handler(num)}
+        >
+            {num}
+        </button>
+    )
+}
+
+
+
+const Card = (
+    {
+        title = content.title,
+        description = content.description,
+        maxRating = content.maxRating
+    }
+) => {
+
+    const [rating, setRating] = useState(null);
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleRating = num => {
+        if (rating !== num)
+            setRating(num);
+    }
+
+    const handleSubmit = () => { setSubmitted(false) };
+
+    const ratingArr = [];
+    for (let i = 0; i < maxRating; i++) {
+        ratingArr.push(
+            <RatingComponent
+                num={i + 1}
+                key={i}
+                handler={handleRating}
+                rating={rating}
+            />
+        )
+    }
+
     return (
         <div className="card__container">
             <div className="card__content">
@@ -18,7 +64,7 @@ const Card = () => {
                     <div className="card__message">
                         <h2 className="card__title">{title}</h2>
                         <p className="card__description">{description}</p>
-                        <div className="card__rating-wrapper"></div>
+                        <div className="card__rating-wrapper">{ratingArr}</div>
                     </div>
                     <button type="button" className="card__cta">submit</button>
                 </div>
@@ -28,7 +74,7 @@ const Card = () => {
                     </div>
                     <div className="card__rating-result-wrapper">
                         <div className="card__rating-result">
-                            You selected {rating} out of {maxRating}
+                            You selected {ratingArr} out of {content.maxRating}
                         </div>
                     </div>
                     <div className="card__thanks-message">

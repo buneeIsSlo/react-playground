2a850a337ba831c527b60b1b42965bbfc945f593
bunee <buneeisslo@gmail.com>
2023-05-06 22:46:20 +0530

    Remove default styles and add brand styles

--- project-tracking-intro/tailwind.config.cjs
+++ project-tracking-intro/tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{jsx, js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          "tomato": "hsl(0, 100%, 68%)",
+          "maritime-blue": "hsl(230, 29%, 20%)",
+          "mysterious-night": "hsl(230, 11%, 40%)",
+          "grey": "hsl(231, 7%, 65%)",
+          "light-grey": "hsl(207, 33%, 95%)"
+        }
+      }
+    },
   },
   plugins: [],
 }

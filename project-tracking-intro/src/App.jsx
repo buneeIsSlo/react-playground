8b95b1ffee447de053ec151d092c4009a3e34a69
bunee <buneeisslo@gmail.com>
2023-05-08 22:55:20 +0530

    Improve mobile design

--- project-tracking-intro/src/App.jsx
+++ project-tracking-intro/src/App.jsx
@@ -2,23 +2,23 @@
 function App() {
 
   return (
-    <div className="app px-8 pt-0.5 min-h-screen bg-brand-light-grey">
+    <div className="app px-8 pt-0.5 min-h-screen bg-white">
       <header className="app__header">
         <a href="#" className="app__logo"></a>
         <div className="app__nav"></div>
       </header>
-      <div className="app__content h-screen grid grid-cols-1 grid-rows-2 outline outline-lime-400">
-        <div className="app__illustartion outline outline-blue-600"></div>
-        <div className="app__intro outline outline-red-400">
-          <div className="app__intro-top">
-            <span className="app__intro-pill">new</span>
-            <span className="text-brand-mysterious-night">monograph dashboard</span>
+      <div className="app__content h-screen grid grid-cols-1 grid-rows-2">
+        <div className="app__illustartion bg-brand-light-grey"></div>
+        <div className="app__intro">
+          <div className="app__intro-top mt-4 flex items-center gap-4">
+            <span className="app__intro-pill text-brand-light-grey font-barlow-condensed font-bold uppercase bg-brand-maritime-blue px-3 py-0.4 rounded-full">new</span>
+            <span className="text-brand-grey font-barlow-condensed uppercase tracking-widest">monograph dashboard</span>
           </div>
-          <h1 className="app__intro-title text-brand-maritime-blue font-barlow-condensed font-black uppercase text-4xl">powerful insights <br></br> into your team</h1>
-          <p className="app__intro-text text-lg font-barlow">Project planning and time tracking for agile teams</p>
-          <div className="app__intro-bot">
-            <button className="app__intro-cta">schedule a demo</button>
-            <span>to see a preview</span>
+          <h1 className="app__intro-title text-brand-maritime-blue mt-4 font-barlow-condensed font-black uppercase text-4xl">powerful insights <br></br> into your team</h1>
+          <p className="app__intro-text text-lg mt-4 text-brand-mysterious-night font-barlow max-w-[28ch]">Project planning and time tracking for agile teams</p>
+          <div className="app__intro-bot mt-4 flex items-center gap-5">
+            <button className="app__intro-cta bg-brand-tomato font-barlow-condensed py-2 px-6 font-bold uppercase text-brand-light-grey rounded-md">schedule a demo</button>
+            <span className="text-brand-grey font-barlow-condensed uppercase">to see a preview</span>
           </div>
         </div>      
       </div>

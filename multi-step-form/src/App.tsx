0fa913036693c89defe0d48dec8219084f719ced
bunee <buneeisslo@gmail.com>
2023-06-12 23:29:47 +0530

    Testing radix-ui form primitive

--- multi-step-form/src/App.tsx
+++ multi-step-form/src/App.tsx
@@ -1,12 +1,24 @@
-import './App.css'
+import "./App.css";
+import * as Form from "@radix-ui/react-form";
 
 function App() {
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline" >Multi step form</h1>
+      <h1 className="text-3xl font-bold underline">Multi step form</h1>
+      <Form.Root>
+        <Form.Field name="text">
+          <Form.Label>Name</Form.Label>
+          <Form.Control />
+        </Form.Field>
+        <Form.Field name="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control />
+        </Form.Field>
+
+        <Form.Submit>Next Step</Form.Submit>
+      </Form.Root>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

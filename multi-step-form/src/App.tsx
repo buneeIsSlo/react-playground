e2bf019278a6c9d028038135f3908d23025d173d
bunee <buneeisslo@gmail.com>
2023-06-17 21:01:42 +0530

    Add: tailwind config and improve styles

--- multi-step-form/src/App.tsx
+++ multi-step-form/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Multi step form</h1>
-      <div className="step-1 bg-slate-400 px-6">
+      <div className="step-1 bg-white px-6">
         <h2 className="text-3xl font-bold">Personal info</h2>
         <p className="text-md">
           Please provide your name, email address, and phone number.
@@ -13,20 +13,43 @@ function App() {
         <Form.Root>
           <Form.Field name="text">
             <Form.Label className="text-sm font-medium block">Name</Form.Label>
-            <Form.Control className="w-full rounded h-10" placeholder="test" />
+            <Form.Control asChild>
+              <input
+                type="text"
+                className="border-neutral-300 w-full rounded border px-4 py-2 text-primary-dark outline-none focus:border-primary"
+                placeholder="text"
+                required
+              ></input>
+            </Form.Control>
           </Form.Field>
           <Form.Field name="email">
             <Form.Label className="text-sm font-medium block">Email</Form.Label>
-            <Form.Control className="w-full rounded h-10" placeholder="test" />
+            <Form.Control asChild>
+              <input
+                type="email"
+                className="border-neutral-300 w-full rounded border px-4 py-2 text-primary-dark outline-none focus:border-primary"
+                placeholder="text"
+                required
+              ></input>
+            </Form.Control>
           </Form.Field>
           <Form.Field name="phone">
             <Form.Label className="text-sm font-medium block">
               Phone Number
             </Form.Label>
-            <Form.Control className="w-full rounded h-10" placeholder="test" />
+            <Form.Control asChild>
+              <input
+                type="number"
+                className="border-neutral-300 w-full rounded border px-4 py-2 text-primary-dark outline-none focus:border-primary"
+                placeholder="text"
+                required
+              ></input>
+            </Form.Control>
           </Form.Field>
 
-          <Form.Submit>Next Step</Form.Submit>
+          <Form.Submit className="ml-auto rounded px-4 py-2 text-white bg-primary-dark hover:opacity-80 lg:rounded-md lg:px-6 l g:py-3">
+            Next Step
+          </Form.Submit>
         </Form.Root>
       </div>
     </>
--- multi-step-form/tailwind.config.js
+++ multi-step-form/tailwind.config.js
@@ -5,7 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        dark: "hsl(213, 96%, 18%)",
+        DEFAULT: "hsl(243, 100%, 62%)",
+        light: "hsl(228, 100%, 84%)",
+        "very-light": "hsl(206, 94%, 87%)",
+      },
+      secondary: "hsl(354, 84%, 57%)",
+      neutral: {
+        400: "hsl(231, 11%, 63%)",
+        300: "hsl(229, 24%, 87%)",
+        200: "hsl(217, 100%, 97%)",
+        100: "hsl(231, 100%, 99%)",
+      },
+      white: "hsl(0, 0%, 100%)",
+    },
+    extend: {
+      backgroundImage: {
+        "sidebar-sm": "url('./assets/images/bg-sidebar-mobile.svg')",
+        "sidebar-lg": "url('./assets/images/bg-sidebar-desktop.svg')",
+      },
+    },
   },
   plugins: [],
 }

f541fec4f7e12cb746873ab874ffdb15809f5aeb
bunee <buneeisslo@gmail.com>
2023-06-20 23:13:26 +0530

    Test: validataion

--- multi-step-form/src/components/forms/PersonalDetails.tsx
+++ multi-step-form/src/components/forms/PersonalDetails.tsx
@@ -1,5 +1,31 @@
 import * as Form from "@radix-ui/react-form";
 
+interface IInputFieldProps {
+  id: string;
+  name: string;
+  placeholder: string;
+  value: string;
+  errorMessage: string;
+}
+
+const fields = [
+  {
+    id: "name",
+    name: "Name",
+    placeholder: "e.g. Stephen King",
+  },
+  {
+    id: "email",
+    name: "Email Address",
+    placeholder: "e.g. stephenking@lorem.com",
+  },
+  {
+    id: "phoneNumber",
+    name: "Phone Number",
+    placeholder: "e.g. +1 234 567 890",
+  },
+];
+
 export default function PersonalDetails() {
   return (
     <div className="step-1 bg-white px-6">
@@ -43,13 +69,46 @@ export default function PersonalDetails() {
             ></input>
           </Form.Control>
         </Form.Field>
-
-        <Form.Submit asChild>
-          <button className="ml-auto rounded px-4 py-2 text-white bg-primary-dark hover:opacity-80 lg:rounded-md lg:px-6 l g:py-3">
-            Next Step
-          </button>
-        </Form.Submit>
       </Form.Root>
+      <Form.Root>
+        {fields.map((field) => (
+          <InputField
+            key={field.id}
+            {...field}
+            value={"test"}
+            errorMessage={"Invalid"}
+          />
+        ))}
+      </Form.Root>
+    </div>
+  );
+}
+
+function InputField(props: IInputFieldProps) {
+  const { id, name, placeholder, value, errorMessage } = props;
+
+  console.log(id, name, placeholder, value, errorMessage);
+
+  return (
+    <div className="mt-3.5 grid gap-1 lg:mt-7">
+      <Form.Field name={name}>
+        <div className="">
+          <Form.Label className="text-xs font-medium text-primary-dark lg:text-sm">
+            {name}
+          </Form.Label>
+          <Form.Message className="float-right font-bold text-secondary">
+            Invalid input
+          </Form.Message>
+        </div>
+        <Form.Control asChild>
+          <input
+            type="text"
+            className="border-neutral-300 w-full rounded border px-4 py-2 text-primary-dark outline-none focus:border-primary"
+            placeholder="text"
+            required
+          ></input>
+        </Form.Control>
+      </Form.Field>
     </div>
   );
 }

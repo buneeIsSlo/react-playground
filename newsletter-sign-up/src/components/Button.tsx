ab7b370820e6fa00429c043e376ece620d1150d9
bunee <buneeisslo@gmail.com>
2023-07-05 23:08:00 +0530

    Improve Button component props

--- newsletter-sign-up/src/components/Button.tsx
+++ newsletter-sign-up/src/components/Button.tsx
@@ -1,7 +1,17 @@
-export default function Button({ textProp: text }: { textProp: string }) {
+import { ReactNode } from "react";
+
+interface IButtonProps {
+  onClickAction?: () => void;
+  children: ReactNode;
+}
+
+export default function Button({ onClickAction, children }: IButtonProps) {
   return (
-    <button className="bg-secondary-400 text-white text-sm font-bold px-2 py-4 w-full rounded-md transition-all ease-out hover:bg-gradient-to-r from-rose-500 to-[#ff6257] hover:shadow-lg hover:shadow-rose-500/50">
-      {text}
+    <button
+      className="bg-secondary-400 text-white text-sm font-bold px-2 py-4 w-full rounded-md transition-all ease-out hover:bg-gradient-to-r from-rose-500 to-[#ff6257] hover:shadow-lg hover:shadow-rose-500/50"
+      onClick={onClickAction ? onClickAction : undefined}
+    >
+      {children}
     </button>
   );
 }

a2c91e0c3f822913905d932fe48950818ec9d0c2
bunee <buneeisslo@gmail.com>
2023-07-05 23:12:04 +0530

    Fix: container height

--- newsletter-sign-up/src/App.tsx
+++ newsletter-sign-up/src/App.tsx
@@ -4,7 +4,7 @@ import Thanks from "./components/Thanks";
 
 function App() {
   return (
-    <div className="w-full h-full bg-secondary-300 laptop:grid laptop:place-content-center">
+    <div className="w-full h-auto laptop:bg-secondary-300 laptop:h-screen laptop:grid laptop:place-content-center">
       <SingUp />
       {/* <Thanks /> */}
     </div>

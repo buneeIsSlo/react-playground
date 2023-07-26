518d2beb863cef4b496257eca402c63d7fd3c125
bunee <buneeisslo@gmail.com>
2023-07-26 18:14:56 +0530

    Add: keyboard layout and hangman

--- hangman/src/App.tsx
+++ hangman/src/App.tsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
+import { HangmanDrawing } from "./HangmanDrawing";
+import { Keyboard } from "./Keyboard";
+import words from "./wordList.json";
+
+function getWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(getWord);
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
   return (
-    <>
-      <h1>Hello!</h1>
-    </>
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <h1>Hangman 💀</h1>
+      <HangmanDrawing />
+      <div style={{ alignSelf: "stretch" }}>
+        <Keyboard />
+      </div>
+    </div>
   );
 }
 

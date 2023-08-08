18ac238aff2c88f182f06768dbbd001e9accee8f
bunee <buneeisslo@gmail.com>
2023-08-08 22:08:46 +0530

    Add: grids

---
+++ tic-tac-toe/src/Game.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const INITIAL_GAME_STATE = ["", "", "", "", "", "", "", "", ""];
+
+function Game() {
+  let [gameState, setGameState] = useState(INITIAL_GAME_STATE);
+
+  return (
+    <div className="h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500">
+      <h1 className="text-center text-5xl mb-4 font-display text-white">
+        Tic Tac Toe
+      </h1>
+      <div>
+        <div className="grid grid-cols-3 gap-3 mx-auto w-96">
+          {gameState.map((player, index) => (
+            <div
+              className="h-36 border-solid border-4 border-slate-200 font-display text-7xl text-center flex justify-center items-center cursor-pointer"
+              key={index}
+            >
+              {player}
+            </div>
+          ))}
+        </div>
+        <div>Scores Go Here</div>
+      </div>
+    </div>
+  );
+}
+
+export default Game;

2b4c60e1aaac02c16a5fac58336a7e5a03ecd4b1
bunee <buneeisslo@gmail.com>
2023-11-21 23:11:31 +0530

    Add: prettier and tailwind config

--- url-shortner/src/App.tsx
+++ url-shortner/src/App.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <div className="text-5xl">URL Shortener</div>;
+  return (
+    <div className="h-full bg-zinc-800">
+      <h1 className="text-3xl text-neutral-100">Shortly - URL shortener</h1>
+    </div>
+  );
 };
 
 export default App;
--- url-shortner/tailwind.config.js
+++ url-shortner/tailwind.config.js
@@ -5,7 +5,40 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'lg': '768px'
+    },
+    fontFamily: {
+      'poppins': ['poppins', 'system-ui'],
+    },
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+      },
+    },
+    extend: {
+      colors: {
+        primary: {
+          cyan: "hsl(180, 66%, 49%)",
+          violet: "hsl(257, 27%, 26%)"
+        },
+        secondary: {
+          red: "hsl(0, 87%, 67%)"
+        },
+        neutral: {
+          100: "hsl(0, 0%, 75%)",
+          200: "hsl(257, 7%, 63%)",
+          300: "hsl(255, 11%, 22%)",
+          400: "hsl(260, 8%, 14%)",
+        }
+      },
+      backgroundImage: {
+        'form-bg-mobile': "url('/src/assets/images/bg-shorten-mobile.svg')",
+        'boost-bg-mobile': "url('/src/assets/images/bg-boost-mobile.svg')",
+        'form-bg-desktop': "url('/src/assets/images/bg-boost-desktop.svg')",
+        'boost-bg-desktop': "url('/src/assets/images/bg-boost-desktop.svg')",
+      }
+    },
   },
   plugins: [],
 }

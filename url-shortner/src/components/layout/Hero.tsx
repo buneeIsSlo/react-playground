d031817e17433cdcd0a76abc2cd04cff6e5cffe2
bunee <buneeisslo@gmail.com>
2023-11-29 19:26:38 +0530

    Fix: Hero styles

--- url-shortner/src/components/layout/Hero.tsx
+++ url-shortner/src/components/layout/Hero.tsx
@@ -2,26 +2,26 @@ import { illusWorking } from "../../assets/images/index";
 
 const Hero = () => {
   return (
-    <section id="hero" className="w-full overflow-hidden lg:min-h-[80vh]">
-      <div className="lg:just-end container mx-auto flex w-[95%] flex-col justify-center gap-2 bg-red-500 outline lg:relative lg:flex-row-reverse lg:items-center">
+    <section id="hero" className="w-full overflow-hidden">
+      <div className="lg:just-end container mx-auto flex w-[95%] flex-col justify-center lg:relative lg:flex-row-reverse lg:items-center">
         <div className="hero-illus lg:relativ mt-4 grid place-content-center lg:flex-1">
           <img
             src={illusWorking}
-            className="ml-40 block w-[120vw] max-w-[500px] md:mx-auto md:max-w-[550px] lg:-mr-[40em] lg:max-w-[700px] xl:-mr-[30em] xl:max-w-[800px]"
+            className="ml-40 block w-[120vw] max-w-[500px] md:mx-auto md:max-w-[550px] lg:-mr-[20em] lg:max-w-[700px] xl:-mr-[25em] xl:max-w-[800px]"
             alt="Illustration of woman working on the computer"
           />
         </div>
-        <div className="hero-text text-center outline outline-red-300 md:mx-auto md:max-w-[70%] lg:h-fit lg:flex-1 lg:text-left">
-          <h1 className="mt-5 text-4xl font-bold md:text-5xl xl:text-7xl">
+        <div className="text-center md:mx-auto md:max-w-[70%] lg:h-fit lg:flex-1 lg:text-left">
+          <h1 className="mt-5 text-4xl font-bold md:text-5xl lg:max-w-[15ch] lg:leading-[1.2] xl:text-7xl xl:leading-tight">
             More than just shorter links
           </h1>
-          <p className="mx-auto mt-5 max-w-[30ch] text-lg md:max-w-[50ch] xl:ml-0 xl:max-w-[40ch]">
+          <p className="mx-auto mt-5 max-w-[30ch] text-lg md:max-w-[50ch] lg:mt-2 xl:ml-0 xl:max-w-[40ch] xl:text-xl">
             Build your brand&apos;s recognition and get detailed insights on how
             your links are performing.
           </p>
           <a
             href="#"
-            className="mx-auto mt-8 block w-fit rounded-full bg-primary-cyan px-12 py-4 font-bold text-neutral-100 lg:ml-0"
+            className="mx-auto mt-8 block w-fit rounded-full bg-primary-cyan px-12 py-4 font-bold text-neutral-100 lg:ml-0 lg:mt-10"
           >
             Get Started
           </a>

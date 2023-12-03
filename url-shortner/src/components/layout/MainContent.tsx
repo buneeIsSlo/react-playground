abea6957b88cce819b97196b5e9e0cb02b7df07f
bunee <buneeisslo@gmail.com>
2023-12-03 22:27:22 +0530

    Fix: Main content sction sytles

--- url-shortner/src/components/layout/MainContent.tsx
+++ url-shortner/src/components/layout/MainContent.tsx
@@ -2,7 +2,10 @@ import { LinkShortener } from "../ui";
 
 const MainContent = () => {
   return (
-    <section id="shortener" className="min-h-[80vh] bg-blue-500">
+    <section
+      id="shortener"
+      className="relative min-h-[80vh] bg-neutral-100 before:absolute before:left-0 before:top-0 before:h-[15vh] before:w-full before:bg-white before:lg:h-[16vh]"
+    >
       <div className="container mx-auto">
         <LinkShortener />
       </div>
--- url-shortner/src/components/ui/LinkShortener.tsx
+++ url-shortner/src/components/ui/LinkShortener.tsx
@@ -16,11 +16,15 @@ type TShortLink = {
 
 const ShortLink = ({ longUrl, shortUrl }: TShortLink) => {
   return (
-    <div>
-      <p>
-        {longUrl}
-        {shortUrl}
-      </p>
+    <div className="flex flex-col gap-3 rounded-md bg-white p-4 shadow-sm md:flex-row md:items-center md:justify-between md:px-6">
+      <p className="text-neutral-n4">{longUrl}</p>
+      <hr className="md:hidden" />
+      <div className="flex flex-col gap-4 md:flex-row md:items-center md:gap-5">
+        <p className="text-primary-cyan">{shortUrl}</p>
+        <button className="btn-primary w-full rounded-md py-2 md:min-w-[6em] lg:min-w-[6.6m]">
+          Copy
+        </button>
+      </div>
     </div>
   );
 };
@@ -28,12 +32,12 @@ const ShortLink = ({ longUrl, shortUrl }: TShortLink) => {
 const LinkShortener = () => {
   return (
     <div className="shortly pt-8">
-      <div className="shortly-form relative z-0 overflow-hidden rounded-xl bg-primary-violet p-7">
+      <div className="shortly-form relative z-0 overflow-hidden rounded-xl bg-primary-violet p-7 md:p-10 lg:p-14">
         <BackgroundPattern />
-        <form action="/" className="flex flex-col gap-10">
-          <div className="relative">
+        <form action="/" className="flex flex-col gap-10 md:flex-row md:gap-6">
+          <div className="relative flex-1">
             <input
-              className="relative block w-full rounded-md px-2 py-3 placeholder:text-neutral-n1"
+              className="relative block w-full rounded-md px-2 py-3 text-neutral-n4 placeholder:text-neutral-n1 md:px-4 lg:px-6 lg:py-4"
               type="url"
               placeholder="Shorten links here..."
               required
@@ -44,14 +48,17 @@ const LinkShortener = () => {
           </div>
           <button
             type="submit"
-            className="btn-primary block w-full rounded-md py-3"
+            className="btn-primary block w-full flex-[0.2] rounded-md py-3 lg:py-4"
           >
             Shorten it!
           </button>
         </form>
       </div>
       <div className="loader"></div>
-      <div className="shortly-links">
+      <div className="shortly-links my-6 grid grid-cols-1 gap-6">
+        <ShortLink longUrl="longUrl" shortUrl="shortUrl" />
+        <ShortLink longUrl="longUrl" shortUrl="shortUrl" />
+        <ShortLink longUrl="longUrl" shortUrl="shortUrl" />
         <ShortLink longUrl="longUrl" shortUrl="shortUrl" />
       </div>
     </div>
